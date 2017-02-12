6b2e0c1e05ed75852a0091d8e05f5ac7a289011f
Luca Lischetti <luca@sirlisko.com>
2017-02-12 15:25:28 +0000

    Express bootstrap

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var path = require('path')
+
+var app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Express' })
+})
+
+var port = process.env.PORT || 3000
+app.listen(, function () {
+  console.log('Up and running on port: ' + port)
+})
